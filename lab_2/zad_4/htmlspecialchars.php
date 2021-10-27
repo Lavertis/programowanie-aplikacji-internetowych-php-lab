53c0c1f3bd0491b811445320b782f5a83ced5e54
Lavertis <29688908+Lavertis@users.noreply.github.com>
2021-10-27 15:17:21 +0200

    refactor: remove unused style tag

--- lab_2/zad_4/htmlspecialchars.php
+++ lab_2/zad_4/htmlspecialchars.php
@@ -18,8 +18,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"></script>
     <title>Test htmlspecialchars()</title>
-    <style>
-    </style>
 </head>
 <body>
 <div class="container my-4">
