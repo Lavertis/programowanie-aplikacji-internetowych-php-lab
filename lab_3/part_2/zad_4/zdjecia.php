857c9fdc14aa8a7e61e97ce25769c6c2db6d4427
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-10-31 21:35:29 +0100

    fix(lab_3): part 2 zad_4 use newer & simpler function for image scaling

--- lab_3/part_2/zad_4/zdjecia.php
+++ lab_3/part_2/zad_4/zdjecia.php
@@ -1,19 +1,19 @@
 <?php
 function showPhotoLinks()
 {
-    echo '<a href="' . $_GET['pic'] . '">Zdjęcie</a><br/>';
-    echo '<a href="mini-' . $_GET['pic'] . '">Miniatura</a><br/><br/>';
+    echo '<a href="' . $_GET['pic'] . '.jpg">Zdjęcie</a><br/>';
+    echo '<a href="' . $_GET['pic'] . '_thumbnail.jpg">Miniatura</a><br/><br/>';
     echo '<a href="zdjecia.html">Powrót</a>';
 }
 
-function managePhotoUpload()
+function redirectToPhotoLinksPage()
 {
     if (!is_uploaded_file($_FILES["photo"]["tmp_name"]))
         return;
 
     $type = $_FILES["photo"]["type"];
     if ($type == "image/jpeg") {
-        $fileName = doStuffWithPhoto();
+        $fileName = managePhoto();
         header('Content-Type: image/jpeg');
         header("location:zdjecia.php?pic=$fileName");
     } else {
@@ -21,21 +21,20 @@ function managePhotoUpload()
     }
 }
 
-function doStuffWithPhoto(): string
+function managePhoto(): string
 {
-    move_uploaded_file($_FILES['photo']['tmp_name'], './' . $_FILES['photo']['name']);
     $fileName = strtolower($_FILES['photo']['name']);
+    move_uploaded_file($_FILES['photo']['tmp_name'], "./$fileName");
     $maxWidth = $_POST['width']; // szerokość preferowana przez użytkownika
     $maxHeight = $_POST['height']; // wysokość preferowana przez użytkownika
-    list($newW, $newH) = calculateNewDimensions($maxWidth, $maxHeight, $fileName);
-    scalePhoto($newW, $newH, $fileName);
-    return $fileName;
+    list($newW, $newH) = getNewDimensions($maxWidth, $maxHeight, $fileName);
+    createThumbnail($newW, $newH, $fileName);
+    return pathinfo($fileName)["filename"];
 }
 
-function calculateNewDimensions($maxWidth, $maxHeight, $fileName): array
+function getNewDimensions($maxWidth, $maxHeight, $fileName): array
 {
-    $scaleY = 1;
-    $scaleX = 1;
+    $scaleX = $scaleY = 1;
     list($width, $height) = getimagesize($fileName); // pobranie rozmiarów obrazu
     if ($width > $maxWidth) $scaleX = $maxWidth / $width;
     if ($height > $maxHeight) $scaleY = $maxHeight / $height;
@@ -46,19 +45,12 @@ function calculateNewDimensions($maxWidth, $maxHeight, $fileName): array
     return [$newW, $newH];
 }
 
-function scalePhoto($newW, $newH, $fileName)
+function createThumbnail($newW, $newH, $fileName)
 {
-    $random = uniqid('img_'); // wygenerowanie losowej nazwy zdjęcia
-    $tempFileName = $random . '.jpg';
-    copy($fileName, './' . $tempFileName); // utworzenie kopii zdjęcia
-    $new = imagecreatetruecolor($newW, $newH); // czarny obraz
-    $picture = imagecreatefromjpeg($tempFileName);
-    list($width, $height) = getimagesize($tempFileName);
-    imagecopyresampled($new, $picture, 0, 0, 0, 0, $newW, $newH, $width, $height);
-    imagejpeg($new, './mini-' . $fileName, 100);
-    imagedestroy($new);
-    imagedestroy($picture);
-    unlink($tempFileName);
+    $image = imagecreatefromjpeg($fileName);
+    $thumbnail = imagescale($image, $newW, $newH);
+    $name = pathinfo($fileName)["filename"];
+    imagejpeg($thumbnail, $name . "_thumbnail.jpg", 100);
 }
 
 ?>
@@ -75,7 +67,7 @@ function scalePhoto($newW, $newH, $fileName)
     if (isset($_GET['pic']) && !empty($_GET['pic']))
         showPhotoLinks();
     elseif (isset($_POST["save"]) && !isset($_GET["pic"]))
-        managePhotoUpload();
+        redirectToPhotoLinksPage();
     ?>
 </div>
 </body>
