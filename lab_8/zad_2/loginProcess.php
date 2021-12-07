0e3ce147db7fa128c360c344cd658d2e23f2ab67
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-12-07 01:06:02 +0100

    feat(lab_8): task_2 finished

---
+++ lab_8/zad_2/loginProcess.php
@@ -0,0 +1,43 @@
+<?php
+include_once "../classes/Database.php";
+include_once "../classes/User.php";
+include_once "../classes/UserManager.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proces logowania - loginProcess.php</title>
+    <style>
+        input, label {
+            margin: 2px;
+        }
+    </style>
+</head>
+<body>
+<div>
+    <?php
+    $db = new Database("localhost", "root", "", "klienci");
+    $userManager = new UserManager();
+    //    $user = new User("elon", "musk", "Elon Musk", "elon@spacex.com");
+    //    $user = new User("bill", "gates", "Bill Gates", "bill@microsoft.com");
+    //    $user->saveToDB($db);
+
+    if (filter_input(INPUT_GET, "akcja") == "wyloguj") {
+        $userManager->logout($db);
+        header("location:loginProcess.php");
+    } else if (filter_input(INPUT_POST, "submit") == "Zaloguj") {
+        $userId = $userManager->login($db);
+        if ($userId > 0) {
+            header("location:testLogin.php");
+        } else {
+            echo "<p>Błędna nazwa użytkownika lub hasło</p>";
+            $userManager->loginForm();
+        }
+    } else
+        $userManager->loginForm();
+    ?>
+</div>
+</body>
+</html>
---
+++ lab_8/zad_2/testLogin.php
@@ -0,0 +1,37 @@
+<?php
+include_once "../classes/Database.php";
+include_once "../classes/User.php";
+include_once "../classes/UserManager.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proces logowania - testLogin.php</title>
+    <style>
+        input, label {
+            margin: 2px;
+        }
+    </style>
+</head>
+<body>
+<div>
+    <?php
+    session_start();
+    $db = new Database("localhost", "root", "", "klienci");
+
+    $sessionId = session_id();
+    $userId = $db->select("logged_in_users", "userId", "sessionId='$sessionId'");
+    if (!$userId)
+        header("location:loginProcess.php");
+
+    echo "<p><a href='loginProcess.php?akcja=wyloguj'>Wyloguj</a></p>";
+    echo "<h3>Dane zalogowanego użytkownika</h3>";
+    $userData = $db->select("users", "*", "ID=$userId");
+    foreach ($userData as $key => $value)
+        echo "$key = $value<br>";
+    ?>
+</div>
+</body>
+</html>
